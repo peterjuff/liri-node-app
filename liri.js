5585e6274018821df3e23ca9e9c31ca64dbd8898
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2018-02-02 23:51:12 -0600

    progress, but not grabbing json

--- liri.js
+++ liri.js
@@ -1,1 +1,12 @@
 require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var request = require("request");
+
+var fs = require("fs");
+
+var userRequest = process.argv[2];
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
