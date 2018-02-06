95fe55675ef2881009f2b0a3e92fb5d0b549cafa
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2018-02-05 22:49:44 -0600

    clean up

--- liri.js
+++ liri.js
@@ -31,23 +31,42 @@ switch (userRequest) {
     case "movie-this":
         movieThis();
         break;
+
+    case "do-what-it-says":
+        doWhatItSays();
+        break;    
 }
 
 function myTweets() {
     var params = {screen_name: 'Peter J'};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
-      if (!error) {
-        console.log(myTweets);
+      if (error) {
+        console.log("Error" + JSON.stringify());
+        console.log(response);
       }
     });
 }
-//console.log(tweet);
+//console.log(tweets);
+//Errorundefined
+//regenerated access tokens
 
 function spotifyThisSong() {
-    spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    var song = process.argv.slice(3);
+    spotify.search({ type: 'track', query: song, limit: 1}, function(err, data) {
         if ( err ) {
             console.log('Error occurred: ' + err);
             return;
+        }
+
+        else {
+            //console.log(data.tracks.items);
+            console.log("Artist: " + data.tracks.album);
+
+
+        }
+
+        if (process.argv.slice(3) === null || undefined) {
+            console.log(song = "The Sign");
         }
      
     });
@@ -70,11 +89,23 @@ function movieThis() {
             console.log("Plot: " + movieData.Plot);
             console.log("Actors: " + movieData.Actors);
           }
-          if (movie = null || undefined) {
+          if (movie === null || undefined) {
               console.log(movie = "Mr. Nobody");
           }
         });
     
 
 }
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function(error, data)
+    if (error) {
+        return console.log(error);
+      }
+
+    //fs.writeFile to add command to the command line.
+
+}
+
+
 
